661fea4e1c72b407346330964cd4351e1a3fb6f9
Henrywambua243 <henrywambuajnr@gmail.com>
2023-03-18 03:14:03 +0300

    latest build

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,40 @@
 import Head from 'next/head';
+import styles from './contact.module.css';
 
 const Contact = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle form submission logic
+  };
+
   return (
     <div>
       <Head>
-        <title>Contact Us | My Website</title>
+        <title>Contact Us</title>
       </Head>
-      <h1>Contact Us</h1>
-      <p>Feel free to reach out to us at the following phone number:</p>
-      <a href="tel:0715709676">0715709676</a>
+
+      <div className={styles.container}>
+        <h1>Contact Us</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" rows="5" required></textarea>
+          </div>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
   );
 };
