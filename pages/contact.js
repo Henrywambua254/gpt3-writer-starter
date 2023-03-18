86c76bbf193957b91307bc6c6a8146fee172f44a
Henrywambua243 <henrywambuajnr@gmail.com>
2023-03-18 03:35:44 +0300

    latest build

--- pages/contact.js
+++ pages/contact.js
@@ -16,24 +16,25 @@ const Contact = () => {
       <div className={styles.container}>
         <h1>Contact Us</h1>
 
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" required />
-          </div>
+        <form onSubmit={handleSubmit} className="contact-form">
+  <div>
+    <label htmlFor="name">Name:</label>
+    <input type="text" id="name" name="name" required />
+  </div>
 
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
-          </div>
+  <div>
+    <label htmlFor="email">Email:</label>
+    <input type="email" id="email" name="email" required />
+  </div>
 
-          <div>
-            <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" rows="5" required></textarea>
-          </div>
+  <div>
+    <label htmlFor="message">Message:</label>
+    <textarea id="message" name="message" rows="5" required></textarea>
+  </div>
 
-          <input type="submit" value="Submit" />
-        </form>
+  <input type="submit" value="Submit" />
+</form>
+
       </div>
     </div>
   );
