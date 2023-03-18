e9d1af182df70b167d294fe88d41d47eed2ee7ac
Henrywambua243 <henrywambuajnr@gmail.com>
2023-03-18 12:11:48 +0300

    latest build

--- pages/api/freegen.js
+++ pages/api/freegen.js
@@ -5,28 +5,56 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const promptPrefix =
+const basePromptPrefix =
     `
-    You are a helpful professor. You can help the writer achieve anything they request you.
-    
-    Based on the instructions you receive, write a creative and engaging piece of content that follows the guidelines provided. Make sure to include all relevant information and adhere to any specific requirements or preferences given by the user. 
-    
-    Remember to keep the tone and style of the writing consistent and appropriate for the topic and intended audience. Good luck!
-Here are the instructions and requirement the user has given you: ${req.body.userInput}
-Output:
+You are a gpt healper. You follow instructions that you have been given and give an outout.
+Now, generate an overview of how your answer will look like based on the instructions given.
+For instance, if the instructions are about creating a table, you could write what the table will have, columns and what it will have.
+If the instructions are for you to write something, you can give an overview of how the answer will be.
+This is an overview or rather outline. So you are not supposed to give the answer, but an outline that will be used to answer or follow the instructions given better.
+Instructions:
 `;
+const generateAction = async(req, res) => {
+    // Run first prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
-const generateAction = async (req, res) => {
-    const completion = await openai.createCompletion({
+    const baseCompletion = await openai.createCompletion({
         model: 'text-davinci-003',
-        prompt: promptPrefix,
+        prompt: `${basePromptPrefix}${req.body.userInput}`,
         temperature: 0.7,
-        max_tokens: 550,
+        max_tokens: 250,
     });
 
-    const output = completion.data.choices.pop();
+    const basePromptOutput = baseCompletion.data.choices.pop();
 
-    res.status(200).json({ output: output });
+    // I build Prompt #2.
+    const secondPrompt =
+        `
+  Now take the overview and follow what it says you should do to give a nice output.
+  You must follow the overview to the latter and do what it says.
+
+  Title: ${req.body.userInput}
+
+  Overview: ${basePromptOutput.text}
+
+  Output:
+  `
+
+    // I call the OpenAI API a second time with Prompt #2
+    const secondPromptCompletion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${secondPrompt}`,
+        // I set a higher temperature for this one. Up to you!
+        temperature: 0.85,
+        // I also increase max_tokens.
+        max_tokens: 1550,
+    });
+
+    // Get the output
+    const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+    // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+    res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
