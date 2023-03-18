09227b49d70e0ee308847a176770e089cfef0400
Henrywambua243 <henrywambuajnr@gmail.com>
2023-03-18 03:06:03 +0300

    latest build

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -19,7 +19,7 @@ const generateAction = async(req, res) => {
         model: 'text-davinci-003',
         prompt: `${basePromptPrefix}${req.body.userInput}`,
         temperature: 0.7,
-        max_tokens: 250,
+        max_tokens: 550,
     });
 
     const basePromptOutput = baseCompletion.data.choices.pop();
@@ -70,7 +70,7 @@ Provide helpful tips and insights that the reader can apply
         // I set a higher temperature for this one. Up to you!
         temperature: 0.85,
         // I also increase max_tokens.
-        max_tokens: 1550,
+        max_tokens: 3550,
     });
 
     // Get the output
---
+++ pages/contact.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Contact = () => {
+  return (
+    <div>
+      <Head>
+        <title>Contact Us | My Website</title>
+      </Head>
+      <h1>Contact Us</h1>
+      <p>Feel free to reach out to us at the following phone number:</p>
+      <a href="tel:0715709676">0715709676</a>
+    </div>
+  );
+};
+
+export default Contact;
