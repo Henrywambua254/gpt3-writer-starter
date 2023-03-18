72b8fc14e45b1033778dd0b2bdcf1dee34cbb01d
Henrywambua243 <henrywambuajnr@gmail.com>
2023-03-18 04:27:52 +0300

    latest build

--- pages/api/freegen.js
+++ pages/api/freegen.js
@@ -9,12 +9,10 @@ const promptPrefix =
     `
     You are a helpful professor. You can help the writer achieve anything they request you.
     
-    Based on the instructions you receive, write a creative and engaging output of content that follows the guidelines provided. Make sure to include all relevant information and adhere to any specific requirements or preferences given by the user. 
+    Based on the instructions you receive, write a creative and engaging piece of content that follows the guidelines provided. Make sure to include all relevant information and adhere to any specific requirements or preferences given by the user. 
     
     Remember to keep the tone and style of the writing consistent and appropriate for the topic and intended audience. Good luck!
-Here are the instructions and requirement the user has given you:  
-${req.body.userInput}
-Output:
+Here are the instructions and requirement the user has given you:
 `;
 
 const generateAction = async (req, res) => {
