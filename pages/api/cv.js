fcafc391b8c468808ef59ab8dc65b184c47336cc
Henrywambua243 <henrywambuajnr@gmail.com>
2023-04-13 12:21:38 +0300

    latest build

---
+++ pages/api/cv.js
@@ -0,0 +1,29 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "You are a killer and fire cover letter writer. Your cover letters are fire and out of this world. You will help me write killer and fire cover letters that get jobs and spark attention. Here are my details:\nName: Blessed Vera Odumo\nPhone: 0746863374\nEmail: odumobless@gmail.com\nI have a degree in economics from JKUAT, experience from numerous internships too\nHere is the formart of the cover letter:\nName:\n\nPhone:\n\nEmail:\n\nDate:\n\nDear Sir/Madam,\nRE:\nOpening paragraph:\n- This is the first thing the employer sees, so it must be fire and killer yet professional\n- Introduce yourself and express your enthusiasm for the position.\n- Mention your knowledge about the company and explain why you are excited about the opportunity.\n- Highlight how the company's commitment to excellence resonates with you.\n- Mention your knowledge about the company and explain why you are excited about the opportunity.\n- Highlight how your skills and qualifications align with the requirements of the job.\n\nBody paragraphs:\n- Highlight your relevant skills and qualifications that make you a strong fit for the Grants Officer position.\n- Mention any other fire or killer skills that I have that makes me the best fit, must be profressional and eye catching\n- Provide specific examples of how you have excelled in similar roles in the past.\n- Express my motivation to join the company\n- Make the paragraphs here fire\n\nClosing paragraph:\n- Express gratitude for the time and consideration of the hiring manager.\n- Convey your eagerness to hear back and your confidence in exceeding expectations in the role.\n- End the letter with a sincere and professional closing.\n- Close with a promise\n\nSincerely,\n[Your Name]\n\nThis formart is to be followed strictly. Adjus if needed to make it bette, killer and fire. However, you must start with Name, phone, email and so forth\nNote: Do NOT name the paragraphs like \"Opening paragraph:\" \"closing paragraph:\" and so on,\nYou will write the cover letter from the following details:";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.82,
+    max_tokens: 3400,
+     top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/cvpage.js
+++ pages/cvpage.js
@@ -4,41 +4,7 @@ import buildspaceLogo from '../assets/buildspace-logo.png';
 import { useState } from 'react';
 
 const Home = () => {
-  const [name, setName] = useState('');
-  const [address, setAddress] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [fullName, setfullName] = useState('');
-  const [linkedin, setLinkedin] = useState('');
-  const [degree, setDegree] = useState('');
-  const [education, seteducation] = useState('');
-  const [workExperience, setworkExperience] = useState('');
-  const [institution, setInstitution] = useState('');
-  const [major, setMajor] = useState('');
-  const [gpa, setGpa] = useState('');
-  const [graduationDate, setGraduationDate] = useState('');
-  const [company, setCompany] = useState('');
-  const [position, setPosition] = useState('');
-  const [jobDescription, setJobDescription] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [achievements, setAchievements] = useState('');
-  const [technicalSkills, setTechnicalSkills] = useState('');
-  const [softSkills, setSoftSkills] = useState('');
-  const [certificationName, setCertificationName] = useState('');
-  const [issuingOrganization, setIssuingOrganization] = useState('');
-  const [certificationDate, setCertificationDate] = useState('');
-  const [awardName, setAwardName] = useState('');
-  const [awardIssuingOrganization, setAwardIssuingOrganization] = useState('');
-  const [awardDate, setAwardDate] = useState('');
-  const [projectName, setProjectName] = useState('');
-  const [projectDescription, setProjectDescription] = useState('');
-  const [technologiesUsed, setTechnologiesUsed] = useState('');
-  const [projectAchievements, setProjectAchievements] = useState('');
-  const [activityName, setActivityName] = useState('');
-  const [activityDescription, setActivityDescription] = useState('');
-  const [activityRole, setActivityRole] = useState('');
-  const [activityDates, setActivityDates] = useState('');
+  const [userInput, setUserInput] = useState('');
   const [apiOutput, setApiOutput] = useState('');
   const [isGenerating, setIsGenerating] = useState(false);
 
@@ -46,45 +12,12 @@ const Home = () => {
     setIsGenerating(true);
 
     console.log('Calling OpenAI...');
-    const response = await fetch('/api/generate', {
+    const response = await fetch('/api/cv', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ 
-        name,
-        address,
-        email,
-        phone,
-        linkedin,
-        degree,
-        institution,
-        major,
-        gpa,
-        graduationDate,
-        company,
-        position,
-        jobDescription,
-        startDate,
-        endDate,
-        achievements,
-        technicalSkills,
-        softSkills,
-        certificationName,
-        issuingOrganization,
-        certificationDate,
-        awardName,
-        awardIssuingOrganization,
-        awardDate,
-        projectName,
-        projectDescription,
-        technologiesUsed,
-        projectAchievements,
-        activityName,
-        activityDescription,
-        activityRole,
-        activityDates,
-      }),
+      body: JSON.stringify({ userInput }),
     });
 
     const data = await response.json();
@@ -95,133 +28,84 @@ const Home = () => {
     setIsGenerating(false);
   };
 
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title> CV Writer | Henry</title>
+        <title> Cover letter Writer | Blessed </title>
       </Head>
 
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>CV Writer</h1>
+            <h1> Blessed's Cover Letter Writer</h1>
           </div>
+
           <div className="header-subtitle">
-        <h2>Enter your details below:</h2>
-      </div>
-    </div>
-
-    <div className="prompt-container">
-      <div className="input-section">
-        <label htmlFor="fullname">Full Name</label>
-        <input
-          type="text"
-          id="fullname"
-          name="fullname"
-          placeholder="Enter your full name"
-          onChange={(e) => setFullName(e.target.value)}
-          value={fullName}
-        />
-      </div>
-
-      <div className="input-section">
-        <label htmlFor="email">Email Address</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Enter your email address"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </div>
-
-      <div className="input-section">
-        <label htmlFor="phone">Phone Number</label>
-        <input
-          type="tel"
-          id="phone"
-          name="phone"
-          placeholder="Enter your phone number"
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          value={phoneNumber}
-        />
-      </div>
-
-      <div className="input-section">
-        <label htmlFor="address">Address</label>
-        <input
-          type="text"
-          id="address"
-          name="address"
-          placeholder="Enter your address"
-          onChange={(e) => setAddress(e.target.value)}
-          value={address}
-        />
-      </div>
-
-      <div className="input-section">
-        <label htmlFor="work-experience">Work Experience</label>
-        <textarea
-          id="work-experience"
-          name="work-experience"
-          placeholder="Enter your work experience"
-          onChange={(e) => setWorkExperience(e.target.value)}
-          value={workExperience}
-        />
-      </div>
-
-      <div className="input-section">
-        <label htmlFor="education">Education</label>
-        <textarea
-          id="education"
-          name="education"
-          placeholder="Enter your education details"
-          onChange={(e) => setEducation(e.target.value)}
-          value={education}
-        />
-      </div>
-
-      <div className="prompt-buttons">
-        <a
-          className={isGenerating ? 'generate-button loading' : 'generate-button'}
-          onClick={callGenerateEndpoint}
-        >
-          <div className="generate">
-            {isGenerating ? (
-              <span className="loader"></span>
-            ) : (
-              <p>Generate</p>
-            )}
+            <h2>Enter the postition and company.
+            </h2>
+            <p>Here is an example:</p>
+            <p>Position: Strategist </p>
+             <p>Company: University of Nairobi
+            </p>
           </div>
-        </a>
-      </div>
+        </div>
 
-      {apiOutput && (
-        <div className="output">
-          <div className="output-header-container">
-            <div className="output-header">
-              <h3>Generated CV</h3>
+        <div className="prompt-container">
+          <textarea
+            placeholder="Start typing here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Do the magic</p>
+                )}
+              </div>
+            </a>
+          </div>
+
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>You Cover Letter</h3>
+                </div>
+              </div>
+
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
             </div>
-          </div>
-
-          <div className="output-content">
-            <p>{apiOutput}</p>
-          </div>
+          )}
         </div>
-      )}
-    </div>
 
-    <div className="badge-container grow">
-      <a href="/">
-        <div className="badge">
-          <p>Built by Henry Wambua</p>
+        <div className="badge-container grow">
+          <a
+            href="/"
+          >
+            <div className="badge">
+
+              <p>Thanks to Henry Wambua</p>
+            </div>
+          </a>
         </div>
-      </a>
+      </div>
     </div>
-  </div>
-</div>
-);
+  );
 };
 
 export default Home;
